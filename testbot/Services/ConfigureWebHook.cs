7b2b2b53003e85586cc324f69b485cedb16178c6
JmlKhan <jmlkhon17@gmail.com>
2022-02-04 11:18:41 +0500

    set up webhook

--- testbot/Services/ConfigureWebHook.cs
+++ testbot/Services/ConfigureWebHook.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
 using testbot.Model;
 
 namespace testbot.Services
@@ -12,7 +15,7 @@ namespace testbot.Services
     {
         private readonly ILogger<ConfigureWebHook> _logger;
         private readonly IServiceProvider _serviceProvider;
-        private readonly object _botConfig;
+        private readonly BotConfiguration _botConfig;
 
         public ConfigureWebHook(ILogger<ConfigureWebHook> logger,
            IServiceProvider serviceProvider,  
@@ -22,14 +25,40 @@ namespace testbot.Services
             _serviceProvider = serviceProvider;
             _botConfig = configuration.GetSection("BotConfiguration").Get<BotConfiguration>();
         }
-        public Task StartAsync(CancellationToken cancellationToken)
+        public async Task StartAsync(CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            using var scope = _serviceProvider.CreateScope();
+            
+            var botClient = scope.ServiceProvider.GetRequiredService<ITelegramBotClient>();
+
+            var webhookAddress = $@"{_botConfig.HostAddress}/bot/{_botConfig.Token}";
+
+            _logger.LogInformation("Setting webhook");
+
+            await botClient.SendTextMessageAsync(
+                chatId: 1678231744,
+                text: "Bot ishga tushmoqda"
+                );
+
+            await botClient.SetWebhookAsync(
+                url: webhookAddress,
+                allowedUpdates: Array.Empty<UpdateType>(),
+                cancellationToken: cancellationToken);
+                
         }
 
-        public Task StopAsync(CancellationToken cancellationToken)
+        public async Task StopAsync(CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            using var scope = _serviceProvider.CreateScope();
+
+            var botClient = scope.ServiceProvider.GetRequiredService<ITelegramBotClient>();
+
+            _logger.LogInformation("Webhook removing");
+
+            await botClient.SendTextMessageAsync(
+                chatId: 1678231744,
+                text: "Bot uxlamoqda"
+                );
         }
     }
 }
